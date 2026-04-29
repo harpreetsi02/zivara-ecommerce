54966ff1bfeeb90efb01672e57a6b37ac3a0d024
harpreetsi02 <ninjasingh27619.com>
2026-04-29 14:30:46 +0530

    create .env

--- utils/api.js
+++ utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api";
 
 const getToken = () => {
   if (typeof window === "undefined") return null;
