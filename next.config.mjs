fb1ec9c9746bd29d0d50479bfc8fa3279593e9a2
harpreetsi02 <ninjasingh27619.com>
2026-05-14 23:45:06 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://13.49.240.174:8080/api/:path*",
+      },
+    ];
+  },
   reactStrictMode: false,
+
 };
 
 export default nextConfig;
